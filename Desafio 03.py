390d088e8bdeb908b078a1a0fd97408e7c649de4
Hilquias Taveira  Gomes <139517840+hilquiastaveira@users.noreply.github.com>
2025-06-16 17:04:56 -0300

    Add files via upload

---
+++ Desafio 03.py
@@ -0,0 +1,5 @@
+print("=======Desafio 02=======")
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite outro: "))
+soma = numero1 + numero2
+print(f"A soma dos valores é igual a {soma}")
